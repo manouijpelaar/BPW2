158ed54ce84b500c80fd7a8ebaf12f87adccb705
manouijpelaar <manou.ijpelaar@gmail.com>
2019-05-16 11:15:05 +0200

    Game

---
+++ BPW2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // Reference to Rigidbody component called "rb"
+    public Rigidbody rb;
+
+    public float forwardForce = 500f;
+    public float sidewaysForce = 500f;
+
+    // I marked this as FixedUpdate
+    // because we use it to mess with physics
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))// if the player presses key "w"
+        {
+            // add a forward force
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("s"))// if the player presses key "s"
+        {
+            // add a backward force
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("d"))//if the player presses key "d"
+        {
+            //add force to the right
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))//if the player presses key "a"
+        {
+            //add force to the left
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
---
+++ BPW2/Assets/Scripts/camMouseLook.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class camMouseLook : MonoBehaviour {
+
+    Vector2 mouseLook;
+    Vector2 smoothV;
+    public float sensitivity = 4.0f;
+    public float smoothness = 2.0f;
+    public float minAngle = -40.0f;
+    public float maxAngle = 60.0f;
+
+    GameObject player;
+
+    void Start()
+    {
+        player = this.transform.parent.gameObject;
+    }
+
+    void Update()
+    {
+        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity * smoothness, sensitivity * smoothness));
+        smoothV.x = Mathf.Lerp(smoothV.x, mouseDelta.x, 1f / smoothness);
+        smoothV.y = Mathf.Lerp(smoothV.y, mouseDelta.y, 1f / smoothness);
+        mouseLook += smoothV;
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        mouseLook.y = Mathf.Clamp(mouseLook.y, minAngle, maxAngle);
+        player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
+
+    }
+}
