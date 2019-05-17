796378dfab2c15f6c5ca770d34fda388c1154c5a
manouijpelaar <manou.ijpelaar@gmail.com>
2019-05-17 12:36:28 +0200

    Game

---
+++ BPW2/Assets/Scripts/winTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class winTrigger : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject completeLevelScreen;
+    //public TargetVolgorde targetManager;
+
+    void OnTriggerEnter(Collider win)
+    {   // when player collides with winscene, level complete
+        if (win.gameObject.tag == "player")
+        {
+            win.gameObject.GetComponentInParent<Rigidbody>().isKinematic = true;
+            completeLevelScreen.SetActive(true);
+            Debug.Log("You Win!");
+        }
+    }
+}
